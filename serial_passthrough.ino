4b72186e5aec98eb579c4d2a6e81f0cb768283a2
Clayton Watts <cletusw@gmail.com>
2020-06-05 00:04:51 -0700

    Add comments

--- serial_passthrough.ino
+++ serial_passthrough.ino
@@ -5,19 +5,19 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB
   }
   currentBaud = Serial.baud();
-  Serial1.begin(currentBaud);
+  Serial1.begin(currentBaud);    // Set hardware serial baud rate to USB's desired baud rate
 }
 
 void loop() {
-  // Check for baud changes on the USB CDC side
+  // Poll for baud changes on the USB CDC side
   // TODO: Instead of polling, upstream something like LUFA's EVENT_CDC_Device_LineEncodingChanged
   // (@see https://github.com/abcminiuser/lufa/blob/597fbf47cd2551423a231ac747e2f1405cf9306a/LUFA/Drivers/USB/Class/Device/CDCClassDevice.c#L86)
   // in Arduino's CDC library:
   // (@see https://github.com/arduino/ArduinoCore-avr/blob/58081c05e548560d3a60050bbf260ebec5d1e867/cores/arduino/CDC.cpp#L94)
   if (currentBaud != Serial.baud()) {
     currentBaud = Serial.baud();
-    Serial1.flush();
-    Serial1.begin(currentBaud);
+    Serial1.flush();             // Finish sending any pending messages
+    Serial1.begin(currentBaud);  // Set hardware serial baud rate to USB's desired baud rate
   }
 
   if (Serial.available()) {
